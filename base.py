02fd03fc08959c59c0eb433d1c24db86e8f64cce
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-12 18:33:34 +0200

    SQLAlchemy setup creating the database engine, session factory, and declarative base class.

---
+++ base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Creating the database engine."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+
+"""Database URL."""
+mysql_db_url = f"mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+"""Create database engine."""
+engine = create_engine(mysql_db_url)
+
+try:
+    conn = engine.connect()
+    print('db.connected')
+    print('Connection object is :{}'.format(conn))
+except Exception as e:
+    print('db not connected:', e)
+
+"""Create a session factory."""
+Session = sessionmaker(bind=engine)
+
+"""Base class for declarative ORM models."""
+Base = declarative_base()
