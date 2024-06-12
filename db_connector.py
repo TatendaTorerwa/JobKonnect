44fe4ca646856b32caa5fc610ef64fc00a9e20f8
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-12 18:27:56 +0200

    Add db_connector.py for database table creation.

--- db_connector.py
+++ db_connector.py
@@ -1,30 +1,16 @@
-#!/usr/bin/python3
-"""Creating the database engine."""
+#!/usr/bin/env python3
+"""Creating the database engine and tables."""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from .models import Users, Jobs, Applications, Employees
-from config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+from base import Base, engine
+from models.user import User
+from models.job import Job
+from models.application import Application
+from models.employee import Employee
 
-"""Database URL."""
-mysql_db_url = f"mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-"""Create database engine."""
-engine = create_engine(mysql_db_url)
-
-try:
-    conn = engine.connect()
-    print('db.connected')
-    print('Connection object is :{}'.format(conn))
-except Exception as e:
-    print('db not connected:', e)
-
-"""Create a session factory."""
+""" Create a session factory."""
 Session = sessionmaker(bind=engine)
-
-"""Base class for declarative ORM models."""
-Base = declarative_base()
 
 """Create database tables based on the defined models."""
 Base.metadata.create_all(engine)
