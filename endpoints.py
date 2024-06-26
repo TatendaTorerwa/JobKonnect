2a583f58b61bcb50b81a192c5e01f5c5443f5067
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-26 16:51:00 +0200

    turn off debugger.

--- endpoints.py
+++ endpoints.py
@@ -96,7 +96,7 @@ def get_user_route(id):
     return jsonify(user.to_dict())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
 
 
