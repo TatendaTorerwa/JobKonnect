37d0f4b039c15702339f10fd2cb7887ae9db1997
TatendaTorerwa <ttorerwa01@gmail.com>
2024-07-01 15:42:09 +0000

    turn on debugger.

--- endpoints.py
+++ endpoints.py
@@ -155,7 +155,7 @@ def delete_job_by_id(job_id):
         return jsonify({'error': f'Failed to delete job with ID {job_id}'}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
